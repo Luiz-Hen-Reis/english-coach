22e07cf66d5bb79c2bf7b0cec97f5f25930f6e8c
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-30 19:35:27 -0300

    feat: create /chat route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { chat } from './controller';
 
 const app = express();
 
 app.use(express.json());
 
+app.post('/chat', chat);
 
 app.listen(5000, () => {
     console.log('Server listening on port 5000');
