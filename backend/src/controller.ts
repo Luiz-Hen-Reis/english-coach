4585041be6550921083d357717512decbf6cd77a
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-30 19:35:13 -0300

    feat: create chat controller action to handle questions and return answers

---
+++ backend/src/controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import getAnswer from "./services/getAnswer";
+
+export async function chat(req: Request, res: Response) {
+    try {
+        const question = req.body.question;
+
+        if (!question) return res.status(400).json({ error: "No question provided." })
+
+        const answer = await getAnswer(question);
+
+        res.status(200).json({ text: answer });
+
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error." })
+    }
+};
