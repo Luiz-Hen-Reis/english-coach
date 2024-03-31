7aa47de7fe46ab59f8a2bdb4749b39d8ecf2a5ef
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-31 12:20:02 -0300

    feat: apply header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 export default function App() {
   return (
-    <div className="text-3xl font-bold underline text-blue-600">App</div>
+    <div className="flex flex-col justify-between items-center h-full py-8 lg:max-w-[1024px] lg:m-auto">
+      <Header />
+      <main>App</main>
+      <footer>footer</footer>
+    </div>
   )
-}
+};
