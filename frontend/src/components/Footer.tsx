bd60cf44b050923f8ddd06092568d0ddea7f68f8
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-31 15:48:57 -0300

    feat: create logic to send messages on footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,11 +1,19 @@
+import { ChangeEvent } from 'react';
 import Button from "./Button";
 
-export default function Footer() {
+type Props = {
+  handleQuestion: (e: ChangeEvent<HTMLInputElement>) => void;
+  question: string;
+  handleSubmit: () => void;
+}
+
+export default function Footer({ handleQuestion, question, handleSubmit }: Props) {
+
     return (
       <footer className="relative w-full px-6">
         <div className="relative">
-          <input type="text" className="w-full py-5 px-4 bg-[#FFFFFF]/20 rounded-md border border-[#FFFFFF]/50 outline-none" />
-          <Button />
+          <input type="text" value={question} onChange={handleQuestion} className="w-full py-5 px-4 bg-[#FFFFFF]/20 rounded-md border border-[#FFFFFF]/50 outline-none" />
+          <Button handleSubmit={handleSubmit} />
         </div>
       </footer>
     );
