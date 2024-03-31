e6775df6e788158dd8c7fda17047f6cbb5f62c3b
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-31 14:51:31 -0300

    feat: create message component

---
+++ frontend/src/components/Message.tsx
@@ -0,0 +1,15 @@
+type Props = {
+  author: string;
+  text: string;
+}
+
+export default function Message({ author, text }: Props) {
+  return (
+    <div className={`flex flex-col ${author === 'User' ? 'items-end' : 'items-start'}`}>
+      <p className="text-sm mb-1">{author}</p>
+      <div className={`app-green-color rounded-md p-3 shadow-md ${author === 'User' ? '' : 'bg-[#FFFFFF]/20'}`}>
+        <p>{text}</p>
+      </div>
+    </div>
+  )
+}
