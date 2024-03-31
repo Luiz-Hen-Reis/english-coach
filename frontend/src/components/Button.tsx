de3f769a485760f350bb80f4b2caa79b2e3ab657
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-31 15:49:48 -0300

    feat: add handleSubmit function to Button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,6 +1,10 @@
-export default function Button() {
+type Props = {
+  handleSubmit: () => void;
+}
+
+export default function Button({ handleSubmit }: Props) {
   return (
-    <button className="absolute top-1 right-1 mt-2 mr-2 shadow-md">
+    <button className="absolute top-1 right-1 mt-2 mr-2 shadow-md" onClick={handleSubmit}>
         <div className="w-10 h-10 app-green-color rounded-md flex justify-center items-center cursor-pointer">
             <img src="/assets/send-msg-icon.svg" alt="send message" />
         </div>
