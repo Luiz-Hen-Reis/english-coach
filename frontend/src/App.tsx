2aa0e6565b73f7814013062d60daf9f348a510b8
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-31 14:52:14 -0300

    feat: add Chat to main screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Chat from "./components/Chat";
 
 export default function App() {
   return (
     <div className="flex flex-col justify-between items-center h-full py-8 lg:max-w-[1024px] lg:m-auto">
       <Header />
-      <main>App</main>
+      <main className="flex-1 w-full overflow-y-auto my-2">
+        <Chat />
+      </main>
       <Footer />
     </div>
   )
