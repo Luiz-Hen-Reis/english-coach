dacc8d0c3f3cc2af17e16cf39cbf1f8fd117cfe4
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-31 13:29:32 -0300

    feat: create footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+export default function Footer() {
+    return (
+      <footer className="relative w-full px-6">
+        <div className="relative">
+          <input type="text" className="w-full py-5 px-4 bg-[#FFFFFF]/20 rounded-md border border-[#FFFFFF]/50 outline-none" />
+          <Button />
+        </div>
+      </footer>
+    );
+  }
+  
