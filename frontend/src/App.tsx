cad2bf0594e4c55fcb5364a20823803ae64158db
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-31 13:30:00 -0300

    feat: add Footer to main screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div className="flex flex-col justify-between items-center h-full py-8 lg:max-w-[1024px] lg:m-auto">
       <Header />
       <main>App</main>
-      <footer>footer</footer>
+      <Footer />
     </div>
   )
 };
