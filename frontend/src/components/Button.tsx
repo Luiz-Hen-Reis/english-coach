c118cb1ac79f58b9ee003053b25975e85b4cbe07
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-31 14:51:57 -0300

    feat: add shadow to button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button() {
   return (
-    <button className="absolute top-1 right-1 mt-2 mr-2">
+    <button className="absolute top-1 right-1 mt-2 mr-2 shadow-md">
         <div className="w-10 h-10 app-green-color rounded-md flex justify-center items-center cursor-pointer">
             <img src="/assets/send-msg-icon.svg" alt="send message" />
         </div>
