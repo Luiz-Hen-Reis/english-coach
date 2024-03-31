4a09d4fdb9eb1f625bc0e7f2be4fce5ffc857986
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-31 16:20:04 -0300

    fix: fix input style

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer({ handleQuestion, question, handleSubmit }: Props
     return (
       <footer className="relative w-full px-6">
         <div className="relative">
-          <input type="text" value={question} onChange={handleQuestion} className="w-full py-5 px-4 bg-[#FFFFFF]/20 rounded-md border border-[#FFFFFF]/50 outline-none" />
+          <input type="text" value={question} onChange={handleQuestion} className="w-full py-5 pl-4 pr-14 bg-[#FFFFFF]/20 rounded-md border border-[#FFFFFF]/50 outline-none" />
           <Button handleSubmit={handleSubmit} />
         </div>
       </footer>
