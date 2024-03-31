5bc9c57fd3f4c77eb5f7b2ed2bd4f9ad93b26410
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-31 18:19:42 -0300

    fix: fix User to user

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -5,9 +5,9 @@ type Props = {
 
 export default function Message({ author, text }: Props) {
   return (
-    <div className={`flex flex-col ${author === 'User' ? 'items-end' : 'items-start'}`}>
+    <div className={`flex flex-col ${author === 'user' ? 'items-end' : 'items-start'}`}>
       <p className="text-sm mb-1">{author}</p>
-      <div className={`app-green-color rounded-md p-3 shadow-md ${author === 'User' ? '' : 'bg-[#FFFFFF]/20'}`}>
+      <div className={`app-green-color rounded-md p-3 shadow-md ${author === 'user' ? '' : 'bg-[#FFFFFF]/20'}`}>
         <p>{text}</p>
       </div>
     </div>
