44942fcf1246e6f5333f46b8a4443da18bcbcc59
Luiz Henrique <lhcontato2020@gmail.com>
2024-03-31 12:19:47 -0300

    feat: create header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className='flex items-center justify-between w-full border-b-2 border-white pb-6 px-6'>
+        <h1 className="text-xl">English Coach App</h1>
+        <img src="/assets/chatgpt-icon.svg" alt="Chatgpt Icon" />
+    </header>
+  )
+}
